717baf88c2ab9a09e2b75bbb9bcbd3a9c4883aba
jian1219 <jian70227@gmail.com>
2026-02-14 00:01:19 +0800

    Added new file

--- src/pages/Love.jsx
+++ src/pages/Love.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 import img1 from "../images/img1.jpg";
 import img2 from "../images/img2.jpg";
-import img3 from "../images/img3.jpg";
+import img3 from "../images/img33.jpg";
 import img4 from "../images/img4.jpg";
 import img5 from "../images/img5.jpg";
 
